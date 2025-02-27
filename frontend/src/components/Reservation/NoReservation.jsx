9f1e4232c0d8182a2d3d49c4c6aa778c624ee884
Lwant <jaimain671@gmail.com>
2025-02-28 00:04:22 +0700

    Fix the text size at the NoReservation component

--- frontend/src/components/Reservation/NoReservation.jsx
+++ frontend/src/components/Reservation/NoReservation.jsx
@@ -15,7 +15,7 @@ export const NoReservation = () => {
       <h3 className=" sm:text-2xl font-bold drop-shadow-xl flex justify-center items-center">
         Booking Not Found
       </h3>
-      <p className="font-semibold sm:text-sm text-xs flex justify-center items-center">
+      <p className="font-semibold sm:text-sm text-xs flex justify-center items-center text-center">
         You don't have any booking yet! Please make a reservation to see it
         here.
       </p>
