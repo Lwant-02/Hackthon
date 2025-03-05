89a6e28d00ec55c3c058fb8e397d910f43124d91
Lwant <jaimain671@gmail.com>
2025-03-05 21:11:55 +0700

    Remove not use value

--- frontend/src/pages/AccountPage.jsx
+++ frontend/src/pages/AccountPage.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import { useRef, useState } from "react";
 import { motion } from "framer-motion";
 import { CustomButton } from "../components/UI/CustomButton";
 import { ImagePicker } from "../components/UI/ImagePicker";
